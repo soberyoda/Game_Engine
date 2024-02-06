d2cee826af84877d5b2705f458db46417c9fc26f
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-02-06 14:52:40 +0100

    window setup

--- Mario_course/src/main/java/engine/Window.java
+++ Mario_course/src/main/java/engine/Window.java
@@ -1,21 +1,85 @@
 package engine;
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 import lombok.Getter;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+import java.util.Objects;
 
 @Getter
 public class Window {
-    private final double width, height;
+    private final int width, height;
     private final String title;
-    private static Window single_instance = null;
+    private static Window window = null;
+    private long w;
     private Window(){
         width = 1920;
         height = 1080;
         title = "Mario";
     }
     public static synchronized Window getInstance(){
-        if(single_instance == null){
-            single_instance = new Window();
+        if(Window.window == null){
+            Window.window = new Window();
         }
-        return single_instance;
+        return Window.window;
+    }
+    public void run(){
+        System.out.println("Hello World");
+        init();
+        loop();
+        glfwFreeCallbacks(w);
+        glfwDestroyWindow(w);
+        glfwTerminate();
+        Objects.requireNonNull(glfwSetErrorCallback(null)).free();
+    }
+    private void init(){
+        GLFWErrorCallback.createPrint(System.err).set();
+        if(!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        w = glfwCreateWindow(width, height, title, NULL, NULL);
+        if(w == NULL){
+            throw new RuntimeException("Failed to create GLFW window");
+        }
+        glfwSetKeyCallback(w, (w, key, scancode, action, mods) -> {
+            if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE){
+                glfwSetWindowShouldClose(w, true);
+            }
+        });
+
+        try (MemoryStack stack = stackPush()){
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+            glfwGetWindowSize(w, pWidth, pHeight);
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(
+                    w,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+        glfwMakeContextCurrent(w);
+        glfwSwapInterval(1);
+        glfwShowWindow(w);
+    }
+    private void loop() {
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        while ( !glfwWindowShouldClose(w) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            glfwSwapBuffers(w);
+            glfwPollEvents();
+        }
     }
 }
