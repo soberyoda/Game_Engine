faa01301cd97852476f3f7b72130cc577fc1e5bd
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-02-19 11:28:08 +0100

    Level Scene

--- Mario_course/src/main/java/engine/LevelEditorScene.java
+++ Mario_course/src/main/java/engine/LevelEditorScene.java
@@ -6,7 +6,7 @@ public class LevelEditorScene extends Scene {
     private boolean changingScene = false;
     private float timeToChangeScene = 2.0f;
     public LevelEditorScene(){
-        System.out.println("Inside level editor scene ");
+        System.out.println("Inside level editor scene");
     }
 
     @Override
--- Mario_course/src/main/java/engine/LevelScene.java
+++ Mario_course/src/main/java/engine/LevelScene.java
@@ -3,6 +3,9 @@ package engine;
 public class LevelScene extends Scene{
     public LevelScene(){
         System.out.println("inside level scene");
+        Window.getInstance().r = 1.0f;
+        Window.getInstance().g = 1.0f;
+        Window.getInstance().b = 1.0f;
     }
     @Override
     public void update(float dt) {
