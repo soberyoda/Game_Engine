7a0852b46f1998cb0210177dac8d208c1053c250
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-02-27 10:50:26 +0100

    some changes

--- Mario_course/src/main/java/engine/LevelEditorScene.java
+++ Mario_course/src/main/java/engine/LevelEditorScene.java
@@ -3,26 +3,14 @@ package engine;
 import java.awt.event.KeyEvent;
 
 public class LevelEditorScene extends Scene {
-    private boolean changingScene = false;
-    private float timeToChangeScene = 2.0f;
+
     public LevelEditorScene(){
-        System.out.println("Inside level editor scene");
+
     }
 
     @Override
     public void update(float dt) {
-        System.out.println(" " + (1.0f/dt) + "FPS");
-        if(!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)){
-            changingScene = true;
-        }
-        if(changingScene && timeToChangeScene > 0.0f){
-            timeToChangeScene -= dt;
-            Window.getInstance().r -= 5.0f;
-            Window.getInstance().g -= 5.0f;
-            Window.getInstance().b -= 5.0f;
-        }else if(changingScene){
-            Window.changeScene(1);
-        }
+
     }
 
 }
