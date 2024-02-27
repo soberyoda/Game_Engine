4d576ff030e2d575db4f08302aaff1fb98e6a359
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-02-27 11:20:41 +0100

    shaders linking and compilation

--- Mario_course/src/main/java/engine/LevelEditorScene.java
+++ Mario_course/src/main/java/engine/LevelEditorScene.java
@@ -1,11 +1,74 @@
 package engine;
 
+import org.lwjgl.opengl.GL20C;
+
 import java.awt.event.KeyEvent;
 
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20C.glCreateShader;
+
 public class LevelEditorScene extends Scene {
+    private final String vertexShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "layout (location = 0) in vec3 aPos;\n" +
+            "layout (location = 1) in vec4 aColor;\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main(){\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+
+    private final String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main(){\n" +
+            "    color = fColor;\n" +
+            "}";
+
+    private int vertexID, fragmentID, shaderProgram;
 
     public LevelEditorScene(){
 
+    }
+
+    @Override
+    public void init(){
+        vertexID = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexID, vertexShaderSrc);
+        glCompileShader(vertexID);
+        int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
+        if(success == GL_FALSE){
+            int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
+            System.out.println("vertex shader compilation failed");
+            System.out.println(glGetShaderInfoLog(vertexID, len));
+            assert false: " ";
+        }
+        fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentID, fragmentShaderSrc);
+        glCompileShader(fragmentID);
+        success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
+        if(success == GL_FALSE){
+            int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
+            System.out.println("fragment shader compilation failed");
+            System.out.println(glGetShaderInfoLog(fragmentID, len));
+            assert false : " ";
+        }
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexID);
+        glAttachShader(shaderProgram, fragmentID);
+        glLinkProgram(shaderProgram);
+
+        success = glGetProgrami(shaderProgram, GL_LINK_STATUS);
+        if(success == GL_FALSE){
+            int len = glGetProgrami(shaderProgram, GL_INFO_LOG_LENGTH);
+            System.out.println("linking failed");
+            System.out.println(glGetProgramInfoLog(shaderProgram, len));
+            assert false : " ";
+        }
     }
 
     @Override
