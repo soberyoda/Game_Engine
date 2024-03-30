bb45a2adad9e9b2a82f2764e0877e7d11aadd6ee
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-03-30 12:16:11 +0100

    delete test

--- Game/src/test/java/LevelEditorSceneTest.java
+++ Game/src/test/java/LevelEditorSceneTest.java
@@ -1,25 +1,3 @@
-import engine.LevelEditorScene;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
-import static org.lwjgl.opengl.GL20.*;
-
 public class LevelEditorSceneTest {
-//    @Test
-//    public void testShaderCompilationAndLinking() throws IOException {
-//        LevelEditorScene scene = new LevelEditorScene();
-//        scene.init();
-//
-//        int vertexID = scene.getVertexID();
-//        int fragmentID = scene.getFragmentID();
-//        int shaderProgram = scene.getShaderProgram();
-//
-//        int vertexCompileStatus = glGetShaderi(vertexID,GL_COMPILE_STATUS);
-//        int fragmentCompileStatus = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
-//        int linkStatus = glGetProgrami(shaderProgram, GL_LINK_STATUS);
-//        Assertions.assertTrue(vertexCompileStatus != GL_FALSE);
-//
-//    }
+
 }
